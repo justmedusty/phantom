66cd3b17cb6546da987a1ae4e989b3114dd53e8f
dustyn <dustyn_gibb@hotmail.com>
2025-12-11 03:31:22 -0500

    beginning png

--- src/filetype_support/png.rs
+++ src/filetype_support/png.rs
@@ -16,3 +16,13 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
+use crate::file_encoding_support::file_encoding_support::{
+    FileEncoding, FileEncodingFunctionDerivation, FileEncodingMethod, FileEncodingSupport,
+};
+use crate::file_encoding_support::pixel::{embed_lsb_data_left_right, extract_lsb_data_left_right, Pixel};
+use std::fs::File;
+use std::io::{Read, Write};
+use std::mem;
+use std::process::exit;
+
+const PNG_MAGIC : [u8;8] = [0x89,0x50,0x4E,0x47,0x0D,0x0A,0x1A,0x0A];
